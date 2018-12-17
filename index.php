f34c7a2ace8e897c5eeb76d7501aa586d2856d85
manna123 <manna@dispostable.com>
2018-12-17 17:30:46 +0530

    Auto Committed

---
+++ index.php
@@ -0,0 +1,528 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<!-- Mirrored from themesjungle.net/html/prime-cab/home-1.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 12 Dec 2018 10:07:27 GMT -->
+<head>
+        <meta charset="utf-8">
+        <meta name="description" content="Prime Cab HTML5 Responsive Template">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Prime Cab HTML5 Responsive Template</title>
+
+        <!-- Css Files Start -->
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href="css/fontawesome-all.min.css" rel="stylesheet">
+		<link href="css/color.css" rel="stylesheet">
+		<link href="css/owl.carousel.css" rel="stylesheet">
+		<link href="css/responsive.css" rel="stylesheet">
+		<link href="css/icomoon.css" rel="stylesheet">
+		<link href="css/animate.css" rel="stylesheet">
+		<!-- Css Files End -->
+		
+		<!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+
+    </head>
+	<body>
+		<!--Wrapper Content Start-->
+		<div class="tj-wrapper">
+			<div class="loader-outer">
+				<div class="tj-loader">
+					<img src="images/pre-loader.gif" alt="">
+					<h2>Loading...</h2>
+				</div>
+			</div>
+			<header class="tj-header">
+				<!--Header Content Start-->
+				<div class="container">
+					<div class="row">
+						<!--Toprow Content Start-->
+						<div class="col-md-3 col-sm-4 col-xs-12">
+							<!--Logo Start-->
+							<div class="tj-logo">
+								<h1><a href="home-1.html"><img src="images/sydney-logo2.png"></a></h1>
+							</div>
+							<!--Logo End-->
+						</div>
+						
+						<div class="col-md-6 col-sm-4 col-xs-12">
+							<div class="info_box">
+								
+								<div class="info_text">
+									<span class="info_title">Email Us</span>
+									<span><a href="mailto:primecab@booking.com">primecab@booking.com</a></span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-xs-12">
+							
+						<div class="phone">
+						<p>
+						<img src="images/phone-ic.png" alt="phone">
+						<span><a href="tel:0478931260">0478 931 260</a></span></p>
+					   </div>
+					
+					
+					
+					
+						</div>
+						<!--Toprow Content End-->
+					</div>
+				</div>
+				
+				
+			</header>
+			
+			
+			<!--Header Content End-->
+			<!--Header Banner Content Start-->
+			<section class="tj-banner-form">
+			
+				<div class="container">
+				
+					<div class="row">
+					
+					<div class="col-md-8 col-sm-7">
+					<h1 class="text">Book your taxi</h1>
+					</div>
+						<!--Header Banner Caption Content Start-->
+						<div class="col-md-8 col-sm-7">
+						
+						
+						<div class="trip-outer">
+								
+						
+								<!--Banner Tab Content Start-->
+								<div class="tab-content">
+									<div class="tab-pane active" id="one-way">
+									
+										<!--Banner Form Content Start-->
+										<form method="POST" class="trip-type-frm">
+										<div class="row">
+										<div class="col-lg-6"><div class="field-outer">
+											<input type="text" required name="pick_loc" placeholder="Name">
+										</div>
+										</div>
+										
+										<div class="col-lg-6">
+										<div class="field-outer">
+										<input type="text" required name="pick_loc" placeholder="Phone Number">
+										</div>
+										</div>
+										
+										</div>
+										
+										<div class="row">
+										<div class="col-lg-6"><div class="field-outer">
+											<input type="email" required name="pick_loc" placeholder="E-mail">
+										</div>
+										</div>
+										
+										<div class="col-lg-6">
+										<div class="field-outer">
+										<input required type="text" name="pick_loc" placeholder="Pickup Suburb">
+										</div>
+										</div>
+										
+										</div>
+										
+										<div class="row">
+										<div class="col-lg-6"><div class="field-outer">
+											<input required type="text" name="pick_loc" placeholder="Pickup Street">
+										</div>
+										</div>
+										
+										<div class="col-lg-6">
+										<div class="field-outer">
+										<input required type="text" name="pick_loc" placeholder="Pickup Street No.">
+										</div>
+										</div>
+										
+										</div>
+										
+										<div class="row">
+										<div class="col-lg-6"><div class="field-outer">
+											<input required type="text" name="pick_loc" placeholder="Drop Off Suburb">
+										</div>
+										</div>
+										
+										<div class="col-lg-6">
+										<div class="field-outer">
+										<select>
+                                           <option value="volvo">No. Of Passengers</option>
+                                         <option value="saab">1</option>
+                                         <option value="saab">2</option>
+                                         <option value="saab">3</option>
+                                         <option value="saab">4</option>
+                                         <option value="saab">5</option>
+                                         <option value="saab">6</option>
+                                         <option value="saab">7</option>
+                                         <option value="saab">8</option>
+                                         <option value="saab">9</option>
+                                         <option value="saab">10</option>
+                                        
+                                     </select>
+										</div>
+										</div>
+										
+										</div>
+										
+										<div class="row">
+										<div class="col-lg-6">
+										<div class="field-outer">
+										<select>
+                                           <option value="volvo">Vehicle Type</option>
+                                         <option value="saab">Sedan</option>
+                                         <option value="saab">Wagon</option>
+                                         <option value="saab">Maxi (1-7 Seater)</option>
+                                         <option value="saab">Maxi (1-11 Seater)</option>
+                                         
+                                        
+                                     </select>
+										</div>
+										</div>
+										
+										<div class="col-lg-6">
+										<div class="field-outer">
+										<select>
+                                           <option value="volvo">Time Required</option>
+                                         <option value="saab">Now</option>
+                                         <option value="saab">Later</option>
+                                         
+                                         
+                                        
+                                     </select>
+										</div>
+										</div>
+										
+										</div>
+										
+										
+										
+										<div class="row">
+										<div class="col-lg-12">
+										<div class="field-outer">
+										<textarea placeholder="Any Special Instructions" rows="4" cols="50"></textarea>
+										</div>
+										</div>
+										
+										
+										
+										</div>
+											
+											<div class="row text-center">
+											<button type="submit" class="search-btn">BOOK TAXI</button>
+                                            </div>											
+										</form>
+										<!--Banner Form Content End-->
+									</div>
+								</div>
+								<!--Banner Tab Content End-->
+							</div>
+						
+						
+						
+						
+						
+							
+							
+							
+							
+							
+						</div>
+						<!--Header Banner Caption Content End-->
+						<!--Header Banner Form Content Start-->
+						<div class="col-md-4 col-sm-5">
+							
+							
+							<div class="banner-caption">
+								<div class="banner-inner bounceInLeft animated delay-2s">
+									<strong>More recently with desktop publishing software ncluding versions</strong>
+									<h2>Upto 25% off on first booking through your app</h2>
+									
+								</div>
+							</div>
+							
+							
+							
+							
+						</div>
+						<!--Header Banner Form Content End-->
+					</div>
+				</div>
+			</section>
+			<!--Header Banner Content End-->
+			
+			<!--Offer Content Start-->
+			<section class="tj-offers">
+				<div class="row">
+					<!--Offer Box Content Start-->
+					<div class="col-md-3 col-sm-6">
+						<div class="offer-box">
+							<img src="images/offer-icon1.png" alt=""/>
+							<div class="offer-info">
+								<h4>Best Price Guaranteed</h4>
+								<p>A more recently with desktop softy  like aldus page maker.</p>
+							</div>
+						</div>
+					</div>
+					<!--Offer Box Content End-->
+					<!--Offer Box Content Start-->
+					<div class="col-md-3 col-sm-6">
+						<div class="offer-box">
+							<img src="images/offer-icon2.png" alt=""/>
+							<div class="offer-info">
+								<h4>24/7 Customer Care</h4>
+								<p>A more recently with desktop softy  like aldus page maker.</p>
+							</div>
+						</div>
+					</div>
+					<!--Offer Box Content End-->
+					<!--Offer Box Content Start-->
+					<div class="col-md-3 col-sm-6">
+						<div class="offer-box">
+							<img src="images/offer-icon3.png" alt=""/>
+							<div class="offer-info">
+								<h4>Home Pickups</h4>
+								<p>A more recently with desktop softy  like aldus page maker.</p>
+							</div>
+						</div>
+					</div>
+					<!--Offer Box Content End-->
+					<!--Offer Box Content Start-->
+					<div class="col-md-3 col-sm-6">
+						<div class="offer-box">
+							<img src="images/offer-icon4.png" alt=""/>
+							<div class="offer-info">
+								<h4>Easy Bookings</h4>
+								<p>A more recently with desktop softy  like aldus page maker.</p>
+							</div>
+						</div>
+					</div>
+					<!--Offer Box Content End-->
+				</div>
+			</section>
+			<!--Offer Content End-->
+			
+			<!--Welcome Section Content Start-->
+			<section class="tj-welcome">
+				<div class="container">
+					<div class="row">
+						<!--Welcome Section Start-->
+						<div class="col-md-6 col-sm-7">
+							<div class="about-info">
+								<div class="tj-heading-style">
+									<h3>Who We Are</h3>
+								</div>
+								<p>Lorem Ipsum passages, and more recently with desktop publishing software like aldus pageMaker including versions of all the Lorem Ipsum generators on thet Internet tends to repeat predefined chunks as necessary, making this an web evolved over the years, sometimes by accident.</p>
+								<ul class="facts-list">
+									<li>
+										<strong class="fact-count">100</strong>
+										<i class="fa fa-percent"></i>
+										<span>Happy Customer</span>
+									</li>
+									<li>
+										<strong class="fact-count">200</strong>
+										<i class="fas fa-plus"></i>
+										<span>Luxury Cars</span>
+									</li>
+									<li>
+										<strong class="fact-count">12,000</strong>
+										<i class="fas fa-arrow-up"></i>
+										<span>Kilometers Driven</span>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!--Welcome Section End-->
+						<!--Welcome Section Image Box Start-->
+						<div class="col-md-6 col-sm-5">
+							<div class="welcome-banner">
+								<img src="images/welcome-img.jpg" alt=""/>
+							</div>
+						</div>
+						<!--Welcome Section Image Box End-->
+					</div>
+				</div>
+			</section>
+			<!--Welcome Section Content End-->
+			
+			<!--Call To Action 2 Content Start-->
+			<section class="tj-cal-to-action2">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-9 col-sm-9">
+							<div class="cta-tagline">
+								<h2>Incredible Destinations at Incredible Deals</h2>
+							</div>
+						</div>
+						
+						<div class="col-md-3 col-sm-3">
+							<div class="cta-btn">
+								<a href="booking-form.html">Book Now <i class="fa fa-arrow-circle-right" aria-hidden="true"></i></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!--Call To Action 2 Content End-->
+			
+			
+
+			<!--Cab Services Section Start-->
+			<section class="cab-services">
+				<div class="container">
+					<div class="row">
+						<!--Cab Services Heading Start-->
+						<div class="tj-heading-style">
+							<h3>Our Services</h3>
+							
+						</div>
+						<!--Cab Services Heading End-->
+						<!--Cab Services Outer Start-->
+						<div class="col-md-4 col-sm-4">
+							<div class="cab-service-box">
+								<!--Cab Services Thumb Start-->
+								<figure class="service-thumb">
+									<img src="images/cab-service-icon1.png" alt=""/>
+								</figure>
+								<!--Cab Services Thumb End-->
+								<!--Cab Services Info Start-->
+								<div class="service-desc">
+									<h4>Restaurants</h4>
+									
+								</div>
+								<!--Cab Services Info End-->
+							</div>
+						</div>
+						<!--Cab Services Outer End-->
+						<!--Cab Services Outer Start-->
+						<div class="col-md-4 col-sm-4">
+							<div class="cab-service-box">
+								<!--Cab Services Thumb Start-->
+								<figure class="service-thumb">
+									<img src="images/cab-service-icon2.png" alt=""/>
+								</figure>
+								<!--Cab Services Thumb End-->
+								<!--Cab Services Info Start-->
+								<div class="service-desc">
+									<h4>Airports</h4>
+									
+								</div>
+								<!--Cab Services Info End-->
+							</div>
+						</div>
+						<!--Cab Services Outer End-->
+						<!--Cab Services Outer Start-->
+						<div class="col-md-4 col-sm-4">
+							<div class="cab-service-box">
+								<!--Cab Services Thumb Start-->
+								<figure class="service-thumb">
+									<img src="images/cab-service-icon3.png" alt=""/>
+								</figure>
+								<!--Cab Services Thumb End-->
+								<!--Cab Services Info Start-->
+								<div class="service-desc">
+									<h4>Hospitals</h4>
+									
+								</div>
+								<!--Cab Services Info End-->
+							</div>
+						</div>
+						<!--Cab Services Outer End-->
+						<!--Cab Services Outer Start-->
+						<div class="col-md-4 col-sm-4">
+							<div class="cab-service-box">
+								<!--Cab Services Thumb Start-->
+								<figure class="service-thumb">
+									<img src="images/cab-service-icon4.png" alt=""/>
+								</figure>
+								<!--Cab Services Thumb End-->
+								<!--Cab Services Info Start-->
+								<div class="service-desc">
+									<h4>Beaches</h4>
+									
+								</div>
+								<!--Cab Services Info End-->
+							</div>
+						</div>
+						<!--Cab Services Outer End-->
+						<!--Cab Services Outer Start-->
+						<div class="col-md-4 col-sm-4">
+							<div class="cab-service-box">
+								<!--Cab Services Thumb Start-->
+								<figure class="service-thumb">
+									<img src="images/cab-service-icon5.png" alt=""/>
+								</figure>
+								<!--Cab Services Thumb End-->
+								<!--Cab Services Info Start-->
+								<div class="service-desc">
+									<h4>Shopping Malls</h4>
+								
+								</div>
+								<!--Cab Services Info End-->
+							</div>
+						</div>
+						<!--Cab Services Outer End-->
+						<!--Cab Services Outer Start-->
+						<div class="col-md-4 col-sm-4">
+							<div class="cab-service-box">
+								<!--Cab Services Thumb Start-->
+								<figure class="service-thumb">
+									<img src="images/cab-service-icon6.png" alt=""/>
+								</figure>
+								<!--Cab Services Thumb End-->
+								<!--Cab Services Info Start-->
+								<div class="service-desc">
+									<h4>Wedding Parties</h4>
+									
+								</div>
+								<!--Cab Services Info End-->
+							</div>
+						</div>
+						<!--Cab Services Outer End-->
+					</div>
+				</div>
+			</section>		
+			<!--Cab Services Section End-->
+					
+		
+					
+			
+			
+			
+			
+			
+			
+		
+			<!--Footer Copyright Start-->
+			<section class="tj-copyright">
+				<div class="container">
+					<div class="row">
+						
+							<p class="text-center">&copy; Copyrights 2018 <a href="home-1.html">Silver Taxi Sydney</a>. All Rights Reserved.</p>
+						
+						
+					</div>
+				</div>
+			</section>
+			<!--Footer Copyright End-->
+		</div>
+		<!--Wrapper Content End-->
+		
+		<!-- Js Files Start --> 
+		<script src="js/jquery-1.12.5.min.js"></script>
+		<script src="js/bootstrap.min.js"></script> 
+		<script src="js/migrate.js"></script>  
+		<script src="js/owl.carousel.min.js"></script>	
+		<script src="js/jquery.counterup.min.js"></script>	
+		<script src="js/waypoints.min.js"></script>
+		<script src="js/tweetie.js"></script>
+		<script src="js/custom.js"></script>
+		<!-- Js Files End --> 
+	</body>
+
+<!-- Mirrored from themesjungle.net/html/prime-cab/home-1.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 12 Dec 2018 10:07:43 GMT -->
+</html>
